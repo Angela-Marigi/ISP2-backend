88d3c14395b4e60cfb573f48fe1291bd32e07581
Angela <46792208+Angela-Marigi@users.noreply.github.com>
2022-11-16 22:08:00 +0300

    Update index.js

--- app/db/mongo/index.js
+++ app/db/mongo/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const {MONGO_HOST, MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE ,MONGO_PORT} = process.env;
 
-const connection = mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,{
+//added uri and removed user, host etc
+//const connection = mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`,{
+//added uri and removed MONGOHOST etc
+
+const uri = 'mongodb+srv://Angela:January2001@cluster0.d08wzxc.mongodb.net/massenger?retryWrites=true&w=majority'
+
+const connection = mongoose.connect(uri,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
